7ce1d462b90630136d6a492e00bddbb95ec243d2
FAHRI_DW <fahriwicaksono011@gmail.com>
2024-10-03 08:31:12 +0700

    Debugging entah.py

--- entah.py
+++ entah.py
@@ -1,8 +1,8 @@
 from random import randint
 
 luck = randint(1-10)
-if(luck<=20): print("Maaf bang kesialan akan menimpamu")
-elif(luck<=50): print("Keberuntungan anda lagi tidak baik")
-elif(luck<=70): print("Lumayan keberuntungan anda normal")
-elif(luck<=90): print("Keberuntungan ada ditangan anda")
+if(luck<=2): print("Maaf bang kesialan akan menimpamu")
+elif(luck<=5): print("Keberuntungan anda lagi tidak baik")
+elif(luck<=7): print("Lumayan keberuntungan anda normal")
+elif(luck<=9): print("Keberuntungan ada ditangan anda")
 else: print("Geloooo, Hari ini lu bakal beruntung banget brooo")
